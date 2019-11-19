2cbf952b8ade41b45ec232836fe6b366cd4ff21b
ideammsoft <ideakslee@gmail.com>
2019-11-19 18:03:01 +0900

    16material design

--- server.js
+++ server.js
@@ -6,9 +6,7 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/api/hello', (req, res) => {
-res.send({ message: 'Hello Express!' });
-});
+
 
 const data = fs.readFileSync('./database.json');
 const conf = JSON.parse(data);
@@ -38,48 +36,29 @@ app.get('/api/customers',(req,res)=>{
 app.use('/image', express.static('./upload'));
 
 app.post('/api/customers', upload.single('image'), (req, res) => {
-
-    let sql = 'INSERT INTO customer VALUES (null, ?, ?, ?, ?, ?,now(),1)';
-    
+    let sql = 'INSERT INTO customer VALUES (null, ?, ?, ?, ?, ?, now(), 0)';
     let image = '/image/' + req.file.filename;
-    
     let name = req.body.name;
-    
     let birthday = req.body.birthday;
-    
     let gender = req.body.gender;
-    
     let job = req.body.job;
-    
     let params = [image, name, birthday, gender, job];
-    
     connection.query(sql, params,
-    
-    (err, rows, fields) => {
-    
-    res.send(rows);
-    
-    }
-    
+        (err, rows, fields) => {
+        res.send(rows);
+        }
     )
-    
     });
     
-    app.delete('/api/customers/:id', (req, res) => {
-        let sql = 'update customer set isDeleted = 1 where id = ?';
-        let params = [req.params.id];
-
-        connection.query(sql, params,
-    
-            (err, rows, fields) => {
-            
-            res.send(rows);
-            
-            }
-        
-        )
-        
-        });
+app.delete('/api/customers/:id', (req, res) => {
+    let sql = 'update customer set isDeleted = 1 where id = ?';
+    let params = [req.params.id];
+    connection.query(sql, params,
+        (err, rows, fields) => {
+        res.send(rows);
+        }
+    )
+    });
     
 
 app.listen(port, () => console.log (`listening on port ${port}`));
